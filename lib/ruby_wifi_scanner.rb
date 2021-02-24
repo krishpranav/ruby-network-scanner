c3f38bfe65d388c59b32fd484861a6efe86e52f4
krishpranav <krisna.pranav@gmail.com>
2021-02-24 16:19:42 +0530

    done changes

--- lib/ruby_wifi_scanner.rb
+++ lib/ruby_wifi_scanner.rb
@@ -1,1 +1,11 @@
 require "ruby_wifi_scanner/version"
+require "ruby_wifi_scanner/scanner"
+require "ruby_wifi_scanner/osx_scanner"
+require "ruby_wifi_scanner/linux_scanner"
+
+module RubyWifiScanner
+    class Error < StandardErrod; end
+    class NotImplemented < Error; end
+    #your code goes here
+
+end
