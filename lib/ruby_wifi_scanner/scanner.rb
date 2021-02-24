749e8fae6ae9d3142f2a4f92f27a0f0d005efe04
krishpranav <krisna.pranav@gmail.com>
2021-02-24 16:42:07 +0530

    done changes

--- lib/ruby_wifi_scanner/scanner.rb
+++ lib/ruby_wifi_scanner/scanner.rb
@@ -15,4 +15,54 @@ module RubyWifiScanner
 		def self.osx?
 			RUBY_PLATFORM =~ /darwin/ 
 		end
-		
+
+
+		private_class_method
+		def self.linux?
+			RUBY_PLATFORM =~ /linux/
+		end
+
+		def initialize(raw_scan = nil)
+			@raw_scan = raw_scan || self.raw_scan
+			@networks = parse(@raw_scan).sort_by { |w| -w.level }
+		end
+
+		def deep_scan
+			ssids = @network.map(&:ssid).uniq
+			@networks = []
+
+			ssids.each do |ssid|
+				@networks += parse directed_scan ssid
+			end
+		end
+
+		def print_fmt freq: nil **kargs
+			@network.select do |n|
+				if freq.nil?
+					true
+				else
+					channel, _ = n.channel.split ?,
+
+					if freq == '2'
+						channel.to_i <= 14
+					else
+						channel.to_i >= 32 && channel.to_i <= 73
+					end
+				end
+
+			end.map do |n|
+				"#{n.rssi}\t#{n.level}\t#{n.channel}\t#{n.ssid}"
+			end
+		end
+
+		protected
+
+		def parse
+			raise NotImplemented
+		end
+
+		def raw_scan
+			raise NotImplemented	
+		end
+	end
+	
