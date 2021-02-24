df100544b9f0dc4808cb6558644ffe3f8b89e03e
krishpranav <krisna.pranav@gmail.com>
2021-02-24 16:49:30 +0530

    done changes

--- test/ruby_wifi_scanner_test.rb
+++ test/ruby_wifi_scanner_test.rb
@@ -1,1 +1,19 @@
 #!/usr/bin/env ruby
+
+require "test_helper"
+require "ruby_wifi_scanner/scanner"
+require "ruby_wifscanner/osx_scanner"
+require "ruby_wifi_scanner/linux_scanner"
+
+
+class RubyWifiScannerTest
+    def test_that_it_has_version_number
+        refute_nil ::RubyWifiScanner::VERSION
+    end
+
+    def test_scanner
+        rws = RubyWifiScanner::Scanner.create
+        assert rws.network.first.level < rws.networks.last.level
+    end
+
+    def test_osx
